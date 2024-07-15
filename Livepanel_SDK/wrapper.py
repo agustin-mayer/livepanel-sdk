d1c69dad0bb8a64c3f1c59cfc184d1cf3cd0e688
mayeragustin99 <mayeragustin99@gmail.com>
2024-07-15 16:14:13 -0300

    update urls staging

--- Livepanel_SDK/wrapper.py
+++ Livepanel_SDK/wrapper.py
@@ -5,21 +5,21 @@ class APIAccess:
         self.auth = auth
 
     def create_project(self, payload): 
-        url = 'https://tools.api.livepanel.ai/v2/projects'
+        url = 'https://tools.api.stg.livepanel.ai/v2/projects'
         headers = self.auth.get_headers()
         response = requests.post(url, headers=headers, json=payload)
         return response.json()
     
 
     def get_project(self, project_id):
-        url = f'https://tools.api.livepanel.ai/v2/projects/{project_id}'
+        url = f'https://tools.api.stg.livepanel.ai/v2/projects/{project_id}'
         headers = self.auth.get_headers()
         response = requests.get(url, headers=headers)
         return response.json()
     
 
     def download_dataset(self, project_id, file_type):
-        url = f'https://tools.api.livepanel.ai/v2/api/projects/{project_id}/datasets/get_file?type={file_type}'
+        url = f'https://tools.api.stg.livepanel.ai/v2/api/projects/{project_id}/datasets/get_file?type={file_type}'
         headers = self.auth.get_headers()
         response = requests.get(url, headers=headers)
         return response.json()
