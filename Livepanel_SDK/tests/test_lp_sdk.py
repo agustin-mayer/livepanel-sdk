84248dc8f1851897a5e014153caf7d3757d68e63
mayeragustin99 <mayeragustin99@gmail.com>
2024-07-15 16:14:42 -0300

    Update test_lp_sdk.py

--- Livepanel_SDK/tests/test_lp_sdk.py
+++ Livepanel_SDK/tests/test_lp_sdk.py
@@ -1,8 +1,50 @@
-import unittest
 from Livepanel_SDK import LivepanelAuth, APIAccess
+from dotenv import load_dotenv
+import os
 
-class TestLivepSDK(unittest.TestCase):
-    #Aca se testearia
+load_dotenv()
 
-    if __name__ == '__main__':
-        unittest.main()
+def get_api_access():
+    api_key = os.getenv('LIVEPANEL_API_KEY')
+    auth = LivepanelAuth(api_key)
+    api_access = APIAccess(auth)
+    return api_access
+
+def test_get_projects():
+    api_access = get_api_access()
+
+    response = api_access.get_projects()
+    print('Get Project Response:', response)
+
+def test_create_project():
+    api_access = get_api_access()
+
+    payload = {
+        'name': 'Proyecto de Prueba',
+        'description': 'Descripción del proyecto de prueba',
+    }
+
+    response = api_access.create_project(payload)
+    print('Create Project Response:', response)
+
+def test_get_project():
+    api_access = get_api_access()
+
+    project_id = 'id_del_proyecto'
+    response = api_access.get_project(project_id)
+    print('Get Project Response:', response)
+
+def test_download_dataset():
+    api_access = get_api_access()
+
+    project_id = 'id_del_proyecto'
+    file_type = 'csv'
+    response = api_access.download_dataset(project_id, file_type)
+    print('Download Dataset Response:', response)
+
+
+if __name__ == '__main__':
+    test_get_projects()
+    # test_create_project()
+    # test_get_project()
+    # test_download_dataset()
