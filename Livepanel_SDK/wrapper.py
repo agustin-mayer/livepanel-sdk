bfcd46f5e9d108825db3de683ee42a0b767836f3
Agustín Mayer <mayeragustin99@gmail.com>
2024-07-23 21:51:49 -0300

    Update wrapper.py

--- Livepanel_SDK/wrapper.py
+++ Livepanel_SDK/wrapper.py
@@ -5,27 +5,27 @@ class APIAccess:
         self.auth = auth
         
     def get_projects(self):
-        url = f'https://tools.api.stg.livepanel.ai/v2/projects'
+        url = f'https://tools.api.stg.livepanel.ai/api/v2/projects'
         headers = self.auth.get_headers()
-        response = requests.get(url, headers=headers, verify=False)
+        response = requests.get(url, headers=headers)
         return response.json()
 
     def create_project(self, payload): 
-        url = 'https://tools.api.stg.livepanel.ai/v2/projects'
+        url = 'https://tools.api.stg.livepanel.ai/api/v2/projects'
         headers = self.auth.get_headers()
         response = requests.post(url, headers=headers, json=payload)
         return response.json()
     
 
     def get_project(self, project_id):
-        url = f'https://tools.api.stg.livepanel.ai/v2/projects/{project_id}'
+        url = f'https://tools.api.stg.livepanel.ai/api/v2/projects/{project_id}'
         headers = self.auth.get_headers()
         response = requests.get(url, headers=headers)
         return response.json()
     
 
     def download_dataset(self, project_id, file_type):
-        url = f'https://tools.api.stg.livepanel.ai/v2/api/projects/{project_id}/datasets/get_file?type={file_type}'
+        url = f'https://tools.api.stg.livepanel.ai/api/v2/projects/{project_id}/datasets/get_file?type={file_type}'
         headers = self.auth.get_headers()
         response = requests.get(url, headers=headers)
         return response.json()
